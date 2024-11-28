95ba85bfa10e3c6e544250d6ba8764c8640cc9fb
Владимир Карпенко <akademik.karpenko@yandex.ru>
2024-11-28 20:55:05 +0300

    add authorization route

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,6 +1,8 @@
 from typing import TypedDict
-from flask import Blueprint
-
+from flask import Blueprint, make_response, request
+from errors import AlreadyExistsError, NotFoundError
+from models.base import Session
+from services import auth as auth_service
 
 auth = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -11,7 +13,22 @@ class AuthData(TypedDict):
 
 
 @auth.route('/authorization', methods=["POST"])
-def authorization(): ...
+def authorization():
+    try:
+        user_data: AuthData = request.get_json()
+        info = auth_service.authorize_user(
+            Session=Session,
+            username=user_data['username'],
+            password=user_data['password'],
+        )
+        response = make_response(info)
+        return response, 200
+    except (ValueError, AlreadyExistsError) as e:
+        return str(e), 400
+    except NotFoundError as e:
+        return str(e), 404
+    except KeyError:
+        return 'Обязательные параметры пустые', 400
 
 
 @auth.route('/registration', methods=["POST"])
