b7cf5c72a3060d8e8afce67172d508fe7e766ff9
murloc1231 <akademik.karpenko@yandex.ru>
2024-12-17 16:12:23 +0300

    fix header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import General from './pages/General'
 import SavedArticles from './pages/SavedArticles'
+import Header from './components/Header'
 
 function App() {
 	return (
 		<Router>
+			<Header/>
 			<Routes>
 				<Route path="/" element={<General />} />
 				<Route path="/favorites" element={<SavedArticles />} />
