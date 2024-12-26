05cacf1e71ff9f8007ed6b250e4dd0dc07b1e1f2
DAVR18 <145141059+DAVR18@users.noreply.github.com>
2024-12-26 08:45:08 +0000

    add get_app func

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,16 @@ from flask_cors import CORS
 from app.models.core import create_tables, drop_tables
 from app.routers import articles
 
-app = Flask(__name__)
-CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-app.register_blueprint(articles.articles_router)
 
-if __name__ == '__main__':
+def get_app():
+    app = Flask(__name__)
+    CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
+    app.register_blueprint(articles.articles_router)
     drop_tables()
     create_tables()
-    app.run(debug=True, port=3010)
+    return app
+
+
+if __name__ == '__main__':
+    get_app().run(debug=True, port=3010)
