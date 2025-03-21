b0d23ea843a258b2f85f0fd275a16d46ca422ddb
murloc1231 <akademik.karpenko@yandex.ru>
2025-03-21 18:27:13 +0300

    send marks to server

--- frontend/src/components/CollectionCard.jsx
+++ frontend/src/components/CollectionCard.jsx
@@ -1,14 +1,26 @@
-import { Card, Typography } from '@mui/material'
-import React from 'react'
+import { Card, Typography, IconButton } from '@mui/material'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-function CollectionCard({ name, articlesCount }) {
+
+function CollectionCard({ name, articlesCount, setCollectionName }) {
 	return (
 		<Card className="collection_card">
 			<Link to={`/user/collections/${name}`}>
 				<Typography variant="h5">{name}</Typography>
 			</Link>
 			<Typography>Сохранено статей: {articlesCount}</Typography>
+			{name !== 'Дизлайки' && name !== 'Лайки' &&
+			<> 
+				<IconButton
+					className="cross_button"
+					onClick={() => setCollectionName(name)}
+				>
+					╳
+				</IconButton>
+				
+			</>
+			}
 		</Card>
 	)
 }
--- frontend/src/pages/CollectionsPage.jsx
+++ frontend/src/pages/CollectionsPage.jsx
@@ -1,14 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Input, Button } from '@mui/material'
+import { Input, Button, Modal, Box, Typography } from '@mui/material'
 import { useForm } from 'react-hook-form'
 import './CollectionsPage.css'
 import { getObjectStoresInfo, addObjectStore, initDB, deleteDB } from '../db'
 import CollectionCard from '../components/CollectionCard'
 import { AppContext } from '../context/AppContext'
+import { deleteCollection } from '../db'
 
 function CollectionsPage() {
 	const { register, handleSubmit, reset, formState } = useForm()
 	const [collections, setCollections] = useState(null)
+	const [collectionForDelete, setCollectionForDelete] = useState('')
 	const { isDbInitialized } = useContext(AppContext)
 
 	function createCollection(data) {
@@ -23,6 +25,8 @@ function CollectionsPage() {
 
 		getObjectStoresInfo().then((info) => setCollections(info))
 	}, [isDbInitialized])
+
+	const handleClose = () => setCollectionForDelete('')
 
 	return (
 		<div className="collections_page">
@@ -55,11 +59,26 @@ function CollectionsPage() {
 							key={collection.name}
 							name={collection.name}
 							articlesCount={collection.articlesCount}
+							setCollectionName={setCollectionForDelete}
 						/>
 					))}
 				</div>
 			)}
+
+				<Modal open={Boolean(collectionForDelete)} onClose={handleClose}>
+					<Box className="modal_content" sx={{display: 'flex', flexDirection: 'column', gap: '40px', alignItems: 'center'}}>
+						<Typography variant='h4'>Все оценки пропадут</Typography>
+						<Button color='error' variant='outlined' onClick={async () => {
+							await deleteCollection(collectionForDelete)
+							setCollections(prev => prev.filter(collection => collection.name !== collectionForDelete))
+							console.log(collections);
+							
+							setCollectionForDelete('')
+						}}>Удалить</Button>
+					</Box>
+				</Modal>
 		</div>
+		
 	)
 }
 
