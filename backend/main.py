3e813037d3d35af199bc6f88f5713311a1338c57
DAVR18 <145141059+DAVR18@users.noreply.github.com>
2025-03-21 21:05:30 +0000

    add add_user route

--- backend/main.py
+++ backend/main.py
@@ -2,17 +2,26 @@ from flask import Flask
 from flask_cors import CORS
 from app.models.core import create_tables, drop_tables
 from app.routers import articles
+from app.models.base import Session
+import uuid
+from app.models.models import Users
+
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.register_blueprint(articles.articles_router)
 
 
-def get_app():
-    app = Flask(__name__)
-    CORS(app)
-    app.config['CORS_HEADERS'] = 'Content-Type'
-    app.register_blueprint(articles.articles_router)
-    drop_tables()
-    create_tables()
-    return app
+@app.post('/add_user')
+def add_user():
+    with Session() as session:
+        user_id = uuid.uuid4()
+        session.add(Users(id=str(user_id)))
+        session.commit()
+    return str(user_id), 200
 
 
 if __name__ == '__main__':
-    get_app().run(debug=True, port=3010)
+    # drop_tables()
+    # create_tables()
+    app.run(debug=True, port=3010)
