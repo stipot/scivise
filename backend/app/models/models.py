157ccaa6ff67108139ef175e1788c01b6e49908b
DAVR18 <145141059+DAVR18@users.noreply.github.com>
2025-04-04 17:15:34 +0000

    add keywords table

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -25,8 +25,22 @@ users_articles = Table(
     UniqueConstraint('user_id', 'article_id'),
 )
 
+# article_keywords = Table(
+#     'article_keywords',
+#     Base.metadata,
+#     Column('keyword_id', ForeignKey('keywords.id')),
+#     Column('article_id', ForeignKey('articles.id')),
+#     UniqueConstraint('keyword_id', 'article_id'),
+# )
 
-class Users(Base):
+
+class Keyword(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    keyword: Mapped[str] = mapped_column(String(255))
+
+
+class User(Base):
     __tablename__ = "users"
     id: Mapped[str] = mapped_column(primary_key=True, init=True)
 
@@ -50,7 +64,7 @@ class Article(Base):
     )
     category: Mapped[str] = mapped_column(String(40), nullable=False)
     annotation: Mapped[str] = mapped_column(String(1000), init=False, nullable=True)
-    keywords: Mapped[str] = mapped_column(String(255), init=False, nullable=True)
+    # keywords: Mapped[List[Keyword]] = relationship(secondary=article_keywords)
     publication_date: Mapped[str] = mapped_column(String(10))
     content: Mapped[str]
     authors: Mapped[List[Author]] = relationship(secondary=authors_articles)
