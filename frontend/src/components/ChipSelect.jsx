60d6829b8d13a7bc56147396f4cfb222371ff151
murloc1231 <akademik.karpenko@yandex.ru>
2025-04-20 10:48:16 +0300

    change filter select

--- frontend/src/components/ChipSelect.jsx
+++ frontend/src/components/ChipSelect.jsx
@@ -1,23 +1,5 @@
 import React from 'react'
-import {
-	Select,
-	OutlinedInput,
-	Chip,
-	MenuItem,
-	InputLabel,
-	Box,
-} from '@mui/material'
-
-const ITEM_HEIGHT = 48
-const ITEM_PADDING_TOP = 8
-const MenuProps = {
-	PaperProps: {
-		style: {
-			maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-			width: 250,
-		},
-	},
-}
+import { Autocomplete, TextField } from '@mui/material'
 
 function ChipSelect({ name, data, dataField, label, setState, state }) {
 	const handleChange = (event) => {
@@ -30,29 +12,17 @@ function ChipSelect({ name, data, dataField, label, setState, state }) {
 	}
 	return (
 		<>
-			<InputLabel id={`${name}-multiple-chip-label`}>{label}</InputLabel>
-			<Select
-				labelId={`${name}-multiple-chip-label`}
-				id={`${name}-multiple-chip`}
+			<Autocomplete
 				multiple
-				value={state}
+				filterSelectedOptions
+				id={`${name}-multiple-chip`}
+				options={data}
+				disableCloseOnSelect={true}
 				onChange={handleChange}
-				input={<OutlinedInput id={`select-chip-${name}`} label={label} />}
-				renderValue={(selected) => (
-					<Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-						{selected.map((value) => (
-							<Chip key={value} label={value} />
-						))}
-					</Box>
-				)}
-				MenuProps={MenuProps}
-			>
-				{data.map((item) => (
-					<MenuItem key={item} value={item}>
-						{item}
-					</MenuItem>
-				))}
-			</Select>
+				limitTags={2}
+				// getOptionLabel={(option) => option.title}
+				renderInput={(params) => <TextField {...params} label={label} />}
+			/>
 		</>
 	)
 }
